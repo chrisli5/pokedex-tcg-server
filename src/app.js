c12b6d29117d9cf6ca8493378a888da7d4aeb42d
chrisli5 <chrisli41@gmail.com>
2021-03-29 16:06:49 -0700

    Added api key authentication

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('./logger');
 const { NODE_ENV } = require('./config');
 
 const app = express();
@@ -13,6 +14,18 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    next()
+  })
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
